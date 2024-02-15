233817ba146d51cbd6f0885368259750d13b0a42
YUJIHARISH <129919969+YUJIHARISH@users.noreply.github.com>
2024-02-15 12:50:42 +0530

    Create Q1)Area of Circle.java

---
+++ Class_Test_1/Q1)Area of Circle.java
@@ -0,0 +1,8 @@
+class Circle {
+    public static void main(String[] args) {
+        int radius=5;
+        double pi=3.14;
+        double area=pi*radius*radius;
+        System.out.println("The area of circle is: " +area);
+    }
+}
