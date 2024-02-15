58c1b805ee16a05c827249f6fc7af66868efbe25
YUJIHARISH <129919969+YUJIHARISH@users.noreply.github.com>
2024-02-15 21:24:12 +0530

    Create Q3)Diamond Pattern.java

---
+++ Class_Test_3/Q3)Diamond Pattern.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Diamond {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int n = input.nextInt();
+    for(int i = 0; i < n; i++) {
+      for(int j = 1; j < n - i; j++) {
+        System.out.print(" ");
+      }
+      for(int j = 0; j < 2 * i + 1; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    for(int i = n - 2; i >= 0; i--) {
+      for(int j = 1; j < n - i; j++) {
+        System.out.print(" ");
+      } 
+      for(int j = 0; j < 2 * i + 1; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
