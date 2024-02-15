ed8cab6d610bdf9a7277efb08795bfd7bca17ac4
YUJIHARISH <129919969+YUJIHARISH@users.noreply.github.com>
2024-02-15 12:53:40 +0530

    Create Q3)Binary to Decimal.java

---
+++ Class_Test_1/Q3)Binary to Decimal.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String binaryNumber = "1011";
+        int decimalNumber = Integer.parseInt(binaryNumber, 2);
+        System.out.println("The decimal number is: " + decimalNumber);
+    }
+}
