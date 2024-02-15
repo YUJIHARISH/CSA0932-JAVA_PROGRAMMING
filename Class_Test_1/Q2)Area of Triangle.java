4fbf80cf324e12055adccf58094a1403ee2bc1f0
YUJIHARISH <129919969+YUJIHARISH@users.noreply.github.com>
2024-02-15 12:52:55 +0530

    Create Q2)Area of Triangle.java

---
+++ Class_Test_1/Q2)Area of Triangle.java
@@ -0,0 +1,8 @@
+class Triangle {
+    public static void main(String[] args) {
+        int b=4;
+        int h=3;
+        double area=0.5*b*h;
+        System.out.println("The area of triangle is: "+area);
+    }
+}
